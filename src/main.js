e48a084dd667e28c024f4d11d4a204b8952c01e3
Daniel Verejan <danielverejan@users.noreply.github.com>
2020-07-11 19:32:36 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
+
+// nothing
+// nothyinig
+// aspdkjasd paskp odkasp odkasp
+// abrbabr abra brb abr ab rb arb ab
